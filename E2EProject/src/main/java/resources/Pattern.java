aad35136511108b2e5544874e8f9bba965a14d89
nitesh <niteshlachake@gmail.com>
2019-12-08 10:58:02 +0530

    Pattern1

---
+++ E2EProject/src/main/java/resources/Pattern.java
@@ -0,0 +1,22 @@
+package resources;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int n=5;
+	
+	for(int i=0;i<=n;i++)
+	{
+		for(int j=n;j>=0;j--)
+		{
+			System.out.println(j);
+			//n=n-1;
+		}
+		//System.out.println(" ");
+	}
+	
+	}
+
+}
